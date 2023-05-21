df17079871a86512b61aeb9668ea669b771827f4
gitName <a@b.c>
2023-05-21 20:31:02 +0100

    working on azure

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,24 +17,24 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://coxsbazaar.com");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    //await page.type(".search-box__input", "automate");
 
     // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    //const searchResultSelector = ".search-box__link";
+    //await page.waitForSelector(searchResultSelector);
+    //await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    //const textSelector = await page.waitForSelector(
+    //  "text/Customize and automate"
+    //);
+    const fullTitle = await page.title();//await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
