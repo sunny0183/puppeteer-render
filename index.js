9043df6ecba9773ba7a6e867baa6121dbaefe880
gitName <a@b.c>
2024-04-28 00:19:43 +0100

    checkin

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
+const { version } = require("./package.json");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+//const version = process.env.npm_package_version || 'No Version Found!';
+
 
 app.get("/scrape", (req, res) => {
   scrapeLogic(res);
 });
 
 app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+  res.send(`Render Puppeteer server is up and running on Port ${PORT} ! Version : ${version}`);
 });
 
 app.listen(PORT, () => {
--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,9 +1,12 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
+const timeout = delay => new Promise(resolve => setTimeout(resolve, delay));  
 
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
+    headless: true,
     args: [
+      //"--headless=new",
       "--disable-setuid-sandbox",
       "--no-sandbox",
       "--single-process",
@@ -17,23 +20,31 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://coxsbazaar.com");
+    await page.goto("https://smart.storage");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
+
+    //await page.waitForTimeout(5000);
+    await page.screenshot({path: "home.png"});
+
 
     // Type into search box
     //await page.type(".search-box__input", "automate");
 
     // Wait and click on first result
-    //const searchResultSelector = ".search-box__link";
-    //await page.waitForSelector(searchResultSelector);
-    //await page.click(searchResultSelector);
+    const searchResultSelector = "div > h2 > a";
+    await page.waitForSelector(searchResultSelector);
+    await timeout(10000);
+    await page.click(searchResultSelector);
+    
 
     // Locate the full title with a unique string
-    //const textSelector = await page.waitForSelector(
-    //  "text/Customize and automate"
-    //);
+    const textSelector = await page.waitForSelector(
+      "article > div.mb-7.mt-7.flex.justify-center"
+    );
+    await timeout(10000);
+    await page.screenshot({path: "blog.png"});
     const fullTitle = await page.title();//await textSelector.evaluate((el) => el.textContent);
 
     // Print the full title
